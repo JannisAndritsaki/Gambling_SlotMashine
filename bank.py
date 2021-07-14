e74c9d713295e648f884cf15c80b944a55e1e3a5
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-14 04:44:34 -0400

    add bank for players gambling money

---
+++ bank.py
@@ -0,0 +1,13 @@
+#This will be the Bank, where the player's money will be saved
+
+class Bank:
+    def __init__(self, balance):
+        self.balance = balance = 0;
+    balance = 0;
+
+    def add_money(amount, self):
+        self.balance + amount;
+
+
+    def remove_money(amount, self):
+        self.balance - amount;
