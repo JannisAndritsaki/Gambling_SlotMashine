b34dc8931589df201605afe6ba1314a678e9b7a3
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-14 11:05:29 +0200

    Welcome screen addet

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    return "Mofo"
+#This is the main
+def WelcomeScreen():
+    print("---------------------------------------------------------------")
+    print("Welcome to the CASINO. Have fun and go win some money.")
+    print("---------------------------------------------------------------")
+
+
+def Hello():
+    print("hello")
+
+def Tschuess():
+    print("hau ab")
+
+def StartGame(user_input):
+    if user_input == "yes":
+        Hello()
+        return 1
+
+    if user_input == "no":
+        Tschuess()
+        return 0
+
+    else:
+        return 0
+
+
+def Start_Gambling():
+    print("Do you want to start?")
+    print("Type -yes-  or  -no-")
+    start_game = input("-> ")
+    StartGame(start_game)
+
+
 
 
 if __name__ == "__main__":
-    print("Shalom")
+    WelcomeScreen()
+    Start_Gambling()
