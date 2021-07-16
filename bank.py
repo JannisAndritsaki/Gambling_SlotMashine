0aad4482727a9c7637fe53d66f0e234c1e028947
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-16 05:43:06 -0400

    Made Bank a Singleton (Aufgabe: Herr Ebert)

--- bank.py
+++ bank.py
@@ -1,10 +1,23 @@
 #This will be the Bank, where the player's money will be saved
 
 class Bank:
+    __instance = None;
     def __init__(self, balance):
         self.balance = balance;
     balance = 0;
 
+    def __init__(self):
+        if Bank.__instance != None:
+            raise Exception("There can only be one Bank")
+        else:
+            Bank.__instance = self
+
+    def get_instance():
+        if Bank.__instance == None:
+            Bank();
+        return Bank.__instance;
+
+    
 
     def add_money(amount, self):
         self.balance + amount;
